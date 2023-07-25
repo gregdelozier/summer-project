f755967bc88589a6ede06b6f812dd1e37014f733
jsipahio <jsipahio@kent.edu>
2023-07-25 19:26:19 -0400

    I did this after the assignment

---
+++ game/gameEngine.js
@@ -0,0 +1,326 @@
+//--------------------------------------------------------------------
+// Class Definintions
+
+// class to store the ID, logical location, and physical center of
+// each block that makes up the board
+class boardBlock{
+    constructor(i, xp, yp, x_coord, y_coord){
+        this.id = i;
+        this.xpos = xp;
+        this.ypos = yp;
+        this.x = x_coord;
+        this.y = y_coord;
+    }
+}
+
+// uses boardBlocks to represent head, tail
+// body is an array of boardBlocks
+// clr is an rgb color
+class snake{
+    constructor(h, b, t, clr){
+        this.head = h;
+        this.body = b;
+        this.tail = t;
+        this.color = clr;
+    }
+}
+
+
+// uses boardBlocks to represent top, bottom
+// rung is an array of boardBlocks
+// clr is an rgb color
+class ladder{
+    constructor(t, r, b, clr){
+        this.top = t;
+        this.rungs = r;
+        this.bottom = b;
+        this.color = clr;
+    }
+}
+
+// red, green, blue color
+class rgb{
+    constructor(r, g, b){
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+}
+
+//--------------------------------------------------------------------------
+// Logical Glogals
+
+// arrray of board blocks, search by ID, not by index
+let board = [];
+// array of snakes
+let snakes = [];
+// array of ladders
+let ladders = [];
+// set of endpoints of ladders and snakes to prevent duplicates
+let endpoints = new Set();
+// boundaries of board once more things are added
+let boardBounds = {}
+// player names
+let player1Name;
+let player2Name;
+let oldP1Position = 1;
+let oldP2Position = 1;
+let dieVal;
+
+//----------------------------------------------------------------------------
+// Engine Functions
+
+// searches the array of blocks for the one with matching ID
+function getBlockByID(i){
+	try{
+		for(block of board){
+			if(block.id == i){
+				return(block);
+			}
+		}
+	}
+	catch{
+		throw new Error("ERROR: Block request for " + i + " out of bounds. If this error persists contact developers.");
+	}
+}
+
+function rand(min, max){
+	return Math.random() * (max - min) + min;
+}
+
+// generates the heads, tails, and bodies of snakes
+function genSnakes(){
+    for (let i = 0; i < 5; i++){
+        // generate head location
+        let h = Math.round(rand(20, 99));
+        // let head = getBlockByID(h);
+        endpoints.add(h);
+        while(endpoints.size < 1 + i * 2){
+            h -= 1;
+            // head = getBlockByID(h);
+            endpoints.add(h);
+        }
+        // generate tail location
+        let t = Math.round(rand(25, 50));
+        let tail;
+        if (h - t > 2){
+            // tail = getBlockByID(h-t);
+			tail = h - t;
+            endpoints.add(tail);
+            while(endpoints.size < 2 + i * 2){
+                t -= 1;
+                tail = h - t; //getBlockByID(h - t);
+                endpoints.add(tail);
+            }
+        }
+        else{
+            tail = (Math.round(rand(2,5))); //getBlockByID(Math.round(random(2,5)));
+            endpoints.add(tail);
+            while(endpoints.size < 2 + i * 2){
+                tail = Math.round(rand(2,5)); //getBlockByID(Math.round(random(2, 5)));
+                endpoints.add(tail);
+            }
+        }
+        // connect head to tail
+
+        // color
+        let color = [];
+        for(let i = 0; i < 3; i++){
+            color.push(Math.round(rand(0,255)));
+        }
+        //add to snakes
+        let snk = new snake(h, 0, tail, new rgb(color[0], color[1], color[2]));
+        snakes.push(snk);
+    }
+}
+
+// generates the tops, bottoms, and rungs for the ladders
+function genLadders(){
+    for (let i = 0; i < 5; i++){
+        // generate head location
+        let top;
+        let t;
+        t = Math.round(rand(20, 99));
+        //top = getBlockByID(t);
+        endpoints.add(t);
+        while(endpoints.size < 11 + i * 2){
+            t -= 1;
+            //top = getBlockByID(t);
+            endpoints.add(t);
+        }
+        // generate tail location
+        let b; 
+        let bottom;
+        b = Math.round(rand(25, 50));
+        if (t - b > 6){
+            bottom = t - b; // getBlockByID(t - b);
+            endpoints.add(bottom);
+            while (endpoints.size < 12 + i * 2){
+                b -= 1;
+                bottom = t - b; //getBlockByID(t - b);
+                endpoints.add(bottom);
+            }
+        }
+        else{
+            bottom = (Math.round(rand(6, 10)));
+            endpoints.add(bottom);
+            while(endpoints.size < 12 + i * 2){
+                bottom = (Math.round(rand(6, 10)));
+                endpoints.add(bottom);
+            }
+        }
+        // connect head to tail
+
+        // color
+        let color = [];
+        for(let i = 0; i < 3; i++){
+            color.push(Math.round(rand(0,255)));
+        }
+
+        //add to snakes
+        let ldr = new ladder(t, 0, bottom, new rgb(color[0], color[1], color[2]));
+        ladders.push(ldr);
+    }
+}
+
+//-----------------------------------------------------------------------------------------------
+// Player Logic
+
+var currentPlayerIndex = 0;
+let players = [{
+    id: "player1",
+    name: "p1", //player1Name,
+    playerPosition: 1,
+    hasWon: false,
+    color: new rgb(255, 0, 0)
+}, {
+    id: "player2",
+    name: "p2", //player2Name,
+    playerPosition: 1,
+    hasWon: false,
+    color: new rgb(0, 0, 255)
+}];
+
+async function startPlay()
+{
+	let currentPlayerIndex = 0;
+	while(!isGameOver()) {
+		let diceValue = await rollDieAndGetValue();
+        dieVal = diceValue;
+		document.getElementById("ran").text = diceValue;
+		let currentPlayer = players[currentPlayerIndex];
+        if (currentPlayerIndex == 0)
+            oldP1Position = currentPlayer.playerPosition;
+        if (currentPlayerIndex == 1)
+            oldP2Position = currentPlayer.playerPosition;
+		movePlayer(currentPlayer, diceValue);
+		currentPlayerIndex = (currentPlayerIndex+1)%(players.length);
+	}
+	
+	let winningPlayer = getWinningPlayer();
+	
+	let message = " Congratulations, " + winningPlayer.name + " won the game!";
+    if (confirm("Game Over!\n" + message)) {
+        window.location.reload(true);
+    } else {
+        window.location.reload(true);
+    }
+}
+
+function movePlayer(currentPlayer, diceValue) {
+	let playerPosition = currentPlayer.playerPosition;
+	playerPosition = playerPosition + diceValue;
+	if (playerPosition > 100) {
+		playerPosition = currentPlayer.playerPosition;
+	}
+	else {
+		movePlayerOnBoard(currentPlayer, diceValue);
+		checkIfPlayerhasWonAndUpdate(currentPlayer);
+	}
+	//document.getElementById(currentPlayer.id).text = currentPlayer.playerPosition;
+}
+
+function checkIfPlayerhasWonAndUpdate(currentPlayer) {
+	if (currentPlayer.playerPosition === 100) {
+		currentPlayer.hasWon = true;
+	}
+}
+
+function movePlayerOnBoard(currentPlayer, diceValue) {
+	for (let i = 0; i < diceValue; i++) {
+		currentPlayer.playerPosition++;
+		//MovePlayer asynchronously with delay on board
+	}
+	currentPlayer.playerPosition = checkIfPlayersPositionHasSnakeHeadAndGetNewPosition(currentPlayer.playerPosition);
+	currentPlayer.playerPosition = checkIfPlayersPositionHasLadderBottomAndGetNewPosition(currentPlayer.playerPosition);
+	//MovePlayer asynchronously with delay on board
+}
+
+function checkIfPlayersPositionHasLadderBottomAndGetNewPosition(playerPosition) {
+	for (let i = 0; i < ladders.length; i++) {
+		if (ladders[i].bottom == playerPosition) {
+			playerPosition = ladders[i].top;
+		}
+	}
+	
+	return playerPosition;
+}
+
+function checkIfPlayersPositionHasSnakeHeadAndGetNewPosition(playerPosition) {
+	for (let i = 0; i < snakes.length; i++) {
+		if (snakes[i].head == playerPosition) {
+			playerPosition = snakes[i].tail;
+		}
+	}
+	
+	return playerPosition;
+}
+
+async function rollDieAndGetValue() {
+	return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(dieValue());
+    }, 2000);
+  });
+}
+
+function dieValue(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function getWinningPlayer() {
+	for (let i = 0; i < players.length; i++) {
+		if (players[i].hasWon) {
+			return players[i];
+		}
+	}
+}
+	
+function isGameOver() {
+	for (let i = 0; i < players.length; i++) {
+		if (players[i].hasWon) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+//----------------------------------------------------------------------------------
+// Setup Functions
+
+genSnakes();
+genLadders();
+
+//----------------------------------------------------------------------------------
+// Testing Exports
+
+module.exports = {
+    players, 
+    movePlayer, 
+    snakes,
+    ladders,
+    getWinningPlayer,
+    checkIfPlayerhasWonAndUpdate,
+    dieValue
+};
---
+++ game/gameP5.js
@@ -0,0 +1,287 @@
+//---------------------------------------------------------------------------------------
+// P5 Standard Functions
+
+//const { players } = require("./gameEngine");
+
+let p1Img;
+let p2Img;
+
+function setup() {
+    createCanvas(750, 750); 
+    // initialize board logically
+    genBoard();
+    // initialize locations of snakes
+    //genSnakes();
+	//console.log(snakes);
+    // initialize locations of ladders
+    //genLadders();
+
+    // p1Img = loadImage("https://webdev-stark.cs.kent.edu/~jsipahio/game/player-1.png");
+    // p2Img = loadImage("https://webdev-stark.cs.kent.edu/~jsipahio/game/player-2.png");
+
+
+	player1Name = prompt("Please enter player#1 name:");
+	player2Name = prompt("Please enter player#2 name:");
+
+	player1Name = player1Name == "" ? "PLAYER-1" : player1Name;
+	player2Name = player2Name == "" ? "PLAYER-2" : player2Name;
+    players[0].name = player1Name;
+    players[1].name = player2Name;
+}
+
+function draw() {
+  background(220);
+  // draw board visually
+  drawBoard();
+  // draw numbers on board
+  drawNumbers();
+  // draw snakes on board
+  drawSnakes();
+  // draw ladders on board
+  drawLadders();
+  // draw player on board
+  drawPlayers();
+}
+
+//----------------------------------------------------------------------------------------------
+// Board Generation
+
+// maps a logical coordinate to its on board coordinate
+function mapCoordToNum(x, y){
+  if(x % 2){
+    return (x + 1) * 10 - y + 1;
+  }
+  else {
+    return x * 10 + y;
+  }
+}
+
+// generates the logical view and phyical view of the board
+function genBoard(){
+    for (let i = 0; i < 10; i ++){
+        for (let j = 0; j < 10; j ++){
+            let piece;
+            let id = mapCoordToNum(j, i + 1);
+            let x_coord = i;
+            let y_coord = j;
+            let xpos =  0 + width/10 * i + width / 20;
+            let ypos = height - (height/10  * j) - height / 20;
+            piece = new boardBlock(id, xpos, ypos, x_coord, y_coord);
+            board.push(piece);
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------
+// Drawing Functions
+
+// draw players on the board
+function drawPlayers(){
+    if(oldP1Position < players[0].playerPosition)
+        oldP1Position += 1;
+    if(oldP1Position > players[0].playerPosition)
+        oldP1Position -= 1;
+    drawPlayer1(getBlockByID(oldP1Position));
+    if(oldP2Position < players[1].playerPosition)
+        oldP2Position += 1;
+    if(oldP2Position > players[1].playerPosition)
+        oldP2Position -= 1;
+    drawPlayer2(getBlockByID(oldP2Position));
+}
+function drawPlayer1(pos){
+    fill(players[0].color.red, players[0].color.green, players[0].color.blue);
+    rect(pos.xpos, pos.ypos, 25);
+}
+
+function drawPlayer2(pos){
+    fill(players[1].color.red, players[1].color.green, players[1].color.blue);
+    rect(pos.xpos, pos.ypos, 25);
+}
+
+// draws the squares for the board on the screen
+function drawBoard(){
+    stroke(40);
+    fill(255);
+    let cutX = width / 10;
+    let cutY = height / 10
+    let x = width - cutX;
+    let y = height - cutY;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        strokeWeight(4);
+        rect(i * cutX, j * cutY, cutX, cutY);
+      }
+    }
+  }
+
+// draws the correct numbers on the board tiles
+function drawNumbers(){
+    strokeWeight(0.5);
+    fill(10);
+    for(block of board){
+        text(block.id, block.xpos, block.ypos);
+    }
+}
+  
+// draws the snakes
+function drawSnakes(){
+    for(snk of snakes){
+        fill(snk.color.red, snk.color.green, snk.color.blue);
+        circle(getBlockByID(snk.head).xpos, getBlockByID(snk.head).ypos, 20);
+        circle(getBlockByID(snk.tail).xpos, getBlockByID(snk.tail).ypos, 20);
+    }
+}
+
+// draws the ladders
+function drawLadders(){
+
+  for(ldr of ladders){
+      // fill(ldr.color.red, ldr.color.green, ldr.color.blue);
+      // let x = ldr.top.
+      // rect(ldr.top.xpos, ldr.top.ypos + 10, 20);
+      // rect(ldr.bottom.xpos, ldr.bottom.ypos, 20);
+
+      let color = [];
+      for(let i = 0; i < 3; i++){
+          color.push(Math.round(random(0,255)));
+      }
+      drawSingleLadder(ldr, color)
+  }
+}
+
+function intersect_point(point1, point2, point3, point4) {
+  const ua = ((point4[0] - point3[0]) * (point1[1] - point3[1]) - 
+            (point4[1] - point3[1]) * (point1[0] - point3[0])) /
+           ((point4[1] - point3[1]) * (point2[0] - point1[0]) - 
+            (point4[0] - point3[0]) * (point2[1] - point1[1]));
+ 
+ const ub = ((point2[0] - point1[0]) * (point1[1] - point3[1]) - 
+            (point2[1] - point1[1]) * (point1[0] - point3[0])) /
+           ((point4[1] - point3[1]) * (point2[0] - point1[0]) - 
+            (point4[0] - point3[0]) * (point2[1] - point1[1]));
+ 
+ const x = point1[0] + ua * (point2[0] - point1[0]);
+ const y = point1[1] + ua * (point2[1] - point1[1]);
+ 
+ return [x, y]
+}
+
+
+function drawSingleLadder(ladder, color) {
+  // Four corners of the ladder's top
+  let ladder_top_cell = {
+      'row': getBlockByID(ladder.top).y_coord,
+      'col': getBlockByID(ladder.top).x_coord,
+
+      'upper_left': {
+          'x': getBlockByID(ladder.top).xpos - width / 20,
+          'y': getBlockByID(ladder.top).ypos - height / 20
+      },
+
+      'upper_right': {
+          'x': getBlockByID(ladder.top).xpos + width / 20,
+          'y': getBlockByID(ladder.top).ypos - height / 20
+      },
+
+      'lower_left': {
+          'x': getBlockByID(ladder.top).xpos - width / 20,
+          'y': getBlockByID(ladder.top).ypos + width / 20
+      },
+
+      'lower_right': {
+          'x': getBlockByID(ladder.top).xpos + width / 20,
+          'y': getBlockByID(ladder.top).ypos + width / 20
+      },
+
+      'mid_left': {
+          'x': getBlockByID(ladder.top).xpos - width / 20,
+          'y': getBlockByID(ladder.top).ypos
+      },
+
+      'mid_right': {
+          'x': getBlockByID(ladder.top).xpos + width / 20,
+          'y': getBlockByID(ladder.top).ypos
+      }
+  }
+
+  let ladder_bottom_cell = {
+      'row': getBlockByID(ladder.bottom).y_coord,
+      'col': getBlockByID(ladder.bottom).x_coord,
+
+      'upper_left': {
+          'x': getBlockByID(ladder.bottom).xpos - width / 20,
+          'y': getBlockByID(ladder.bottom).ypos - height / 20
+      },
+
+      'upper_right': {
+          'x': getBlockByID(ladder.bottom).xpos + width / 20,
+          'y': getBlockByID(ladder.bottom).ypos - height / 20
+      },
+
+      'lower_left': {
+          'x': getBlockByID(ladder.bottom).xpos - width / 20,
+          'y': getBlockByID(ladder.bottom).ypos + width / 20
+      },
+
+      'lower_right': {
+          'x': getBlockByID(ladder.bottom).xpos + width / 20,
+          'y': getBlockByID(ladder.bottom).ypos + width / 20
+      },
+
+      'mid_left': {
+          'x': getBlockByID(ladder.bottom).xpos - width / 20,
+          'y': getBlockByID(ladder.bottom).ypos
+      },
+
+      'mid_right': {
+          'x': getBlockByID(ladder.bottom).xpos + width / 20,
+          'y': getBlockByID(ladder.bottom).ypos
+      }
+  
+  }
+  
+  stroke([ladder.color.red, ladder.color.green, ladder.color.blue])
+  
+  line(ladder_top_cell.mid_left.x, ladder_top_cell.mid_left.y, 
+          ladder_bottom_cell.mid_left.x, ladder_bottom_cell.mid_left.y);
+
+  line(ladder_top_cell.mid_right.x, ladder_top_cell.mid_right.y, 
+      ladder_bottom_cell.mid_right.x, ladder_bottom_cell.mid_right.y)  
+  
+
+  for(var i = ladder_top_cell.mid_left.y + 10; i < ladder_bottom_cell.mid_left.y; i += 15) {
+      
+      // Line 1
+      // (0, i), (width, i)
+      // Line 2
+      horizontal_point1 = [0, i];
+      horizontal_point2 = [width, i];
+
+      ladder_line1_pt = [
+          [ladder_top_cell.mid_left.x, ladder_top_cell.mid_left.y],
+          [ladder_bottom_cell.mid_left.x, ladder_bottom_cell.mid_left.y]
+      ];
+      
+      ladder_line2_pt = [
+          [ladder_top_cell.mid_right.x, ladder_top_cell.mid_right.y],
+          [ladder_bottom_cell.mid_right.x, ladder_bottom_cell.mid_right.y]
+      ];
+
+      pt1 = intersect_point(horizontal_point1, horizontal_point2, ladder_line1_pt[0], ladder_line1_pt[1]);
+      pt2 = intersect_point(horizontal_point1, horizontal_point2, ladder_line2_pt[0], ladder_line2_pt[1]);
+
+
+      stroke([ladder.color.red, ladder.color.green, ladder.color.blue])
+      line(pt1[0], pt1[1], 
+          pt2[0], pt2[1]);
+
+
+  }
+
+}
+
+//----------------------------------------------------------------------------------
+// Start Game Engine
+
+startPlay();
+
